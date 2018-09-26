909a2e0e93c2f06a7e6cefc7e7a711290d4f7559
GinaKonidaris <ginakonidaris@gmail.com>
2018-09-26 20:19:21 +0200

    Intial commit

---
+++ POEGADE18013130/RangedUnit.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POEGADE18013130
+{
+    class RangedUnit : Unit
+    {
+        private new string Name;
+
+        public new string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        private int Xpos;
+
+        public int xpos
+        {
+            get { return Xpos; }
+            set { Xpos = value; }
+        }
+
+        private int Ypos;
+
+        public int ypos
+        {
+            get { return Ypos; }
+            set { Ypos = value; }
+        }
+
+        private int Health;
+
+        public int health
+        {
+            get { return Health; }
+            set { Health = value; }
+        }
+
+        private int Speed;
+
+        public int speed
+        {
+            get { return Speed; }
+            set { Speed = value; }
+        }
+
+        private int Attack;
+
+        public int attack
+        {
+            get { return Attack; }
+            set { Attack = value; }
+        }
+
+        private int InRanged;
+
+        public int inranged
+        {
+            get { return InRanged; }
+            set { InRanged = value; }
+        }
+
+        private int Faction;
+
+        public int faction
+        {
+            get { return Faction; }
+            set { Faction = value; }
+        }
+        private string Symbol;
+
+        public string symbol
+        {
+            get { return Symbol; }
+            set { Symbol = value; }
+        }
+        public override void Move(Direction d)
+        {
+            switch (d)
+            {
+                case Direction.North:
+                    {
+                        Ypos -= Speed;
+                        break;
+                    }
+                case Direction.East:
+                    {
+                        Xpos += Speed;
+                        break;
+                    }
+                case Direction.West:
+                    {
+                        Xpos -= Speed;
+                        break;
+                    }
+                case Direction.South:
+                    {
+                        Ypos += Speed;
+                        break;
+                    }
+            }
+        }
+
+        public RangedUnit(int x, int y, int speed, int Range, int Health, int Team, string symbol, int attack)
+        {
+
+            Xpos -= x;
+            Ypos = y;
+            Health = health;
+            Speed = speed;
+            Range = range;
+            Team = team;
+            Symbol = symbol;
+            Attack = attack;
+        }
+
+        private int DistanceTo(Unit u)
+        {
+            if (u.GetType() == typeof(MeleeUnit))
+            {
+                MeleeUnit n = (MeleeUnit)u;
+                int d = (Xpos - n.xpos) + Math.Abs(Ypos - n.ypos);
+                return d;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public Direction DirectionTo(Unit u)
+        {
+            if (u.GetType() == typeof(RangedUnit))
+            {
+                RangedUnit n = (RangedUnit)u;
+                if (n.Xpos < n.Ypos)
+                {
+                    return Direction.North;
+                }
+                else if (n.Xpos > Xpos)
+                {
+                    return Direction.South;
+                }
+                else if (n.Ypos < Ypos)
+                {
+                    return Direction.West;
+                }
+                else
+                {
+                    return Direction.East;
+                }
+            }
+            else;
+            {
+                return Direction.North;
+            }
+        }
+        public override void Combat(Unit u)
+        {
+            if (u.GetType() == typeof(RangedUnit))
+            {
+                health -= ((RangedUnit)u).Attack;
+
+            }
+            else if (u.GetType() == typeof(MeleeUnit))
+            {
+                health -= ((MeleeUnit)u).attack;
+                health -= ((MeleeUnit)u).attack;
+
+            }
+
+        }
+        public override bool Inranged(Unit u)
+        {
+            if (u.GetType() == typeof(RangedUnit))
+            {
+                RangedUnit n = (RangedUnit)u;
+                if (DistanceTo(u) <= range)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        public override Unit Closest(Unit[] units)
+        {
+            Unit closest = this;
+            int closestDistance = 50;
+
+            foreach (Unit u in units)
+            {
+                //if (u.GetType() == typeof(MeleeUnit))
+                //{
+                if (((RangedUnit)u).team == team)
+                {
+                    if (DistanceTo(u) < closestDistance)
+                    {
+                        closest = u;
+                        closestDistance = DistanceTo((RangedUnit)u);
+                    }
+                }
+                //}
+                //else if(u.GetType() == typeof(RangedUnit))
+                //{
+                //    if (DistanceTo(u) < closestDistance)
+                //    {
+                //        closest = u;
+                //        closestDistance = DistanceTo((RangedUnit)u);
+                //    }
+                //}
+            }
+            return closest;
+        }
+        public override bool Isdead()
+        {
+            if (health < +0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public override string Tostring()
+        {
+            return "RU" + Xpos + "," + Ypos + "," + health + "," + Name;
+        }
+
+    }
+}
